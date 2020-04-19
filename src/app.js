132f2e19484d0ede915441fbc19ab6ac17abf0e7
Galvao Da Cunha <wgalvao@indracompany.com>
2020-04-19 00:05:57 -0300

    Desafio: Conceitos do React Native

--- src/app.js
+++ src/app.js
@@ -2,49 +2,125 @@ import React, { useState, useEffect } from "react";
 
 import api from "./services/api";
 
-import "./styles.css";
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  FlatList,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
-function App() {
+export default function App() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
     handleListRepository();
   }, []);
 
-  async function handleListRepository(id) {
+  async function handleListRepository() {
     const { data } = await api.get("repositories");
-    if (data) setRepositories(data);
+    setRepositories(data);
   }
 
-  async function handleAddRepository() {
-    const repository = {
-      url: "https://github.com/Rocketseat/umbriel",
-      title: "Umbriel",
-      techs: ["Node", "Express", "TypeScript"],
-    };
-    const { data } = await api.post("repositories", repository);
-    setRepositories([...repositories, data]);
-  }
-
-  async function handleRemoveRepository(id) {
-    await api.delete(`/repositories/${id}`);
+  async function handleLikeRepository(id) {
+    const { data } = await api.post(`/repositories/${id}/like`);
     handleListRepository();
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        {repositories.map(({ id, title }) => (
-          <li key={id}>
-            {title}
-            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
-          </li>
-        ))}
-      </ul>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <ScrollView>
+        <SafeAreaView style={styles.container}>
+          {repositories.map(({ id, title, techs, likes }) => (
+            <View key={id} style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{title}</Text>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+              {techs.map((tech, index) => {
+                const techId = `${tech}-${index}`;
+                return (
+                  <View key={techId} style={styles.techsContainer}>
+                    <Text style={styles.tech}>{tech}</Text>
+                  </View>
+                );
+              })}
+
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${id}`}
+                >
+                  {likes} curtidas
+                </Text>
+              </View>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </SafeAreaView>
+      </ScrollView>
+    </>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+  repositoryContainer: {
+    marginBottom: 15,
+    marginHorizontal: 15,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  repository: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  techsContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  tech: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginRight: 10,
+    backgroundColor: "#04d361",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: "#fff",
+  },
+  likesContainer: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  likeText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#7159c1",
+    padding: 15,
+  },
+});
